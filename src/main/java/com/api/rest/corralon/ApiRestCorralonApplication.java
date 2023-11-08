4a50bc10f6ffa978ab557b02d8420317470c9b55
Ariel-Lamas <seba.lamas.al@gmail.com>
2023-11-07 23:00:57 -0300

    Inicio de proyecto Backend.

---
+++ src/main/java/com/api/rest/corralon/ApiRestCorralonApplication.java
@@ -0,0 +1,13 @@
+package com.api.rest.corralon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCorralonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCorralonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/rest/corralon/ApiRestCorralonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.corralon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestCorralonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
